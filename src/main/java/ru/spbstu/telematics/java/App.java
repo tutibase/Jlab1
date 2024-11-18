ea921f81c920396c22f982501b14df6c26ba9313
tutibase <dykun1784@gmail.com>
2024-11-18 10:50:50 +0300

    feat: added the ability to select text and file to overwrite

--- src/main/java/ru/spbstu/telematics/java/App.java
+++ src/main/java/ru/spbstu/telematics/java/App.java
@@ -2,17 +2,25 @@ package ru.spbstu.telematics.java;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        String filePath = "file.txt";
-        String textToWrite = "New text!";
-        
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.print("Укажите путь до файла: ");
+        String filePath = scanner.nextLine(); 
+
+        System.out.print("Введите текст для записи в файл: "); 
+        String textToWrite = scanner.nextLine(); 
+
         try (FileWriter writer = new FileWriter(filePath, false)) { 
             writer.write(textToWrite);
             System.out.println("Запись в файл завершена.");
         } catch (IOException e) {
             System.err.println("Ошибка при записи в файл: " + e.getMessage());
+        } finally {
+            scanner.close();
         }
     }
 }

c59ba832fb42843f9ad422bf0e373a230112a763
tutibase <dykun1784@gmail.com>
2024-11-30 10:38:26 +0300

    refactor: making writeToFile a separate class function

--- src/main/java/ru/spbstu/telematics/java/App.java
+++ src/main/java/ru/spbstu/telematics/java/App.java
@@ -6,21 +6,25 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
+        Scanner scanner = new Scanner(System.in);
 
         System.out.print("Укажите путь до файла: ");
-        String filePath = scanner.nextLine(); 
+        String filePath = scanner.nextLine();
 
-        System.out.print("Введите текст для записи в файл: "); 
-        String textToWrite = scanner.nextLine(); 
+        System.out.print("Введите текст для записи в файл: ");
+        String textToWrite = scanner.nextLine();
 
-        try (FileWriter writer = new FileWriter(filePath, false)) { 
+        writeToFile(filePath, textToWrite);
+
+        scanner.close();
+    }
+
+    public static void writeToFile(String filePath, String textToWrite) {
+        try (FileWriter writer = new FileWriter(filePath, false)) {
             writer.write(textToWrite);
             System.out.println("Запись в файл завершена.");
         } catch (IOException e) {
             System.err.println("Ошибка при записи в файл: " + e.getMessage());
-        } finally {
-            scanner.close();
         }
     }
 }
